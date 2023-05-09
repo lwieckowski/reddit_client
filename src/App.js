300a862870be30a27b23c18e21d65d0e9ccc2441
lwieckowski <lwieckowski@outlook.com>
2023-05-09 16:04:17 +0200

    Add simple api call

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import { createContext, useEffect, useReducer } from 'react';
+
+const initialState = {
+  posts: [],
+  error: "",
+};
+
+const Context = createContext(initialState);
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'FETCH_POSTS':
+      return {
+        ...state,
+        posts: action.payload,
+      };
+    case 'ERROR':
+      return {
+        ...state,
+        error: action.payload,
+        posts: [],
+      };
+    default:
+      return state;
+  }
+}
+
+async function fetchPosts(dispatch, subreddit) {
+  const response = await fetch(`https://www.reddit.com/r/${subreddit}.json`);
+  if (!response.ok) {
+    dispatch({
+      type: 'ERROR',
+      payload: `Something went wrong! Response status: ${response.status}`,
+    });
+	} else {
+    const data = await response.json();
+    dispatch({
+      type: 'FETCH_POSTS',
+      payload: data.data.children.map((c) => c.data),
+    });
+  }
+}
+
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetchPosts(dispatch, 'reactjs');
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Context.Provider value={{ state, dispatch }}>
+      {state.error && <p>{state.error}</p>}
+      {state.posts.map((post) => <p>{post.title}</p>)}
+    </Context.Provider>
   );
 }
 
