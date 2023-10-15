596caedac44f5a77d833deb30ecb8ec37301c1e8
lwieckowski <lwieckowski@outlook.com>
2023-10-15 14:29:19 +0200

    Refactor get age code

--- src/components/PostListItem.js
+++ src/components/PostListItem.js
@@ -1,32 +1,35 @@
 import { Box, CardActionArea, Card, CardContent, Typography, CardMedia } from "@mui/material";
 
-const bull = "\u2022";
+const SECONDS_IN_MINUTE = 60;
+const MINUTES_IN_HOUR = 60;
+const HOURS_IN_DAY = 24;
+const SECONDS_IN_HOUR = SECONDS_IN_MINUTE * MINUTES_IN_HOUR;
+const SECONDS_IN_DAY = SECONDS_IN_HOUR * HOURS_IN_DAY;
+
+const BULLET = "\u2022";
 
 export function PostListItem({ post }) {
 
-  function getAge(post) {
-    const now = new Date();
+  function getPostAge(post) {
     const created = new Date(post.created_utc * 1000);
-    const secondsAgo = Math.max(Math.floor((now - created) / 1000), 0);
-    if (secondsAgo < 60)
-      return `${secondsAgo} seconds ago`;
-    const minutesAgo = Math.floor(secondsAgo / 60);
-    if (minutesAgo < 60)
-      return `${minutesAgo} minutes ago`;
-    const hoursAgo = Math.floor(minutesAgo / 60);
-    if (hoursAgo < 24)
-      return `${hoursAgo} hours ago`;
-    return `${Math.floor(hoursAgo / 24)} days ago`;
+    const secondsAgo = Math.max(Math.round((new Date() - created) / 1000), 0);
+    if (secondsAgo > SECONDS_IN_DAY)
+      return `${Math.round(secondsAgo / SECONDS_IN_DAY)} days ago`;
+    if (secondsAgo > SECONDS_IN_HOUR)
+      return `${Math.round(secondsAgo / SECONDS_IN_HOUR)} hours ago`;
+    if (secondsAgo > SECONDS_IN_MINUTE)
+      return `${Math.round(secondsAgo / SECONDS_IN_MINUTE)} minutes ago`;
+    return `${secondsAgo} seconds ago`;
   }
 
   return (
     <Card sx={{ minWidth: 275, mt: 1 }}>
-      <CardActionArea sx={{display: 'flex', justifyContent: 'space-between'}}>
+      <CardActionArea sx={{ display: 'flex', justifyContent: 'space-between' }}>
         <Box>
           <CardContent>
             <Typography sx={{ fontSize: 13 }} color="text.secondary">
-              <b>{post.subreddit_name_prefixed}</b> {bull} Posted by {post.author}{" "}
-              {bull} {getAge(post)}
+              <b>{post.subreddit_name_prefixed}</b> {BULLET} Posted by {post.author}{" "}
+              {BULLET} {getPostAge(post)}
             </Typography>
             <Typography
               sx={{ fontSize: 16, mt: 1 }}
@@ -37,16 +40,16 @@ export function PostListItem({ post }) {
               {post.title}
             </Typography>
             <Typography sx={{ fontSize: 13, mt: 1 }} color="text.secondary">
-              {post.ups} upvotes {bull} {post.num_comments} comments {bull}{" "}
+              {post.ups} upvotes {BULLET} {post.num_comments} comments {BULLET}{" "}
               {post.total_awards_received} awards
             </Typography>
           </CardContent>
         </Box>
         <Box>
-        <CardMedia
+          <CardMedia
             sx={{ width: 120, height: 120, m: 1, objectFit: "contain" }}
             image={post.thumbnail}
-        />
+          />
         </Box>
       </CardActionArea>
     </Card>
