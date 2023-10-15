c162289b35c9c8b2dc7ab2eef51cf36db84cfd25
lwieckowski <lwieckowski@outlook.com>
2023-10-15 13:56:28 +0200

    Calculate age more accurately

--- src/components/PostListItem.js
+++ src/components/PostListItem.js
@@ -3,8 +3,21 @@ import { Box, CardActionArea, Card, CardContent, Typography, CardMedia } from "@
 const bull = "\u2022";
 
 export function PostListItem({ post }) {
-  const created = new Date(post.created_utc * 1000);
-  const diff = Math.round((new Date() - created) / 86400000);
+
+  function getAge(post) {
+    const now = new Date();
+    const created = new Date(post.created_utc * 1000);
+    const secondsAgo = Math.max(Math.floor((now - created) / 1000), 0);
+    if (secondsAgo < 60)
+      return `${secondsAgo} seconds ago`;
+    const minutesAgo = Math.floor(secondsAgo / 60);
+    if (minutesAgo < 60)
+      return `${minutesAgo} minutes ago`;
+    const hoursAgo = Math.floor(minutesAgo / 60);
+    if (hoursAgo < 24)
+      return `${hoursAgo} hours ago`;
+    return `${Math.floor(hoursAgo / 24)} days ago`;
+  }
 
   return (
     <Card sx={{ minWidth: 275, mt: 1 }}>
@@ -13,7 +26,7 @@ export function PostListItem({ post }) {
           <CardContent>
             <Typography sx={{ fontSize: 13 }} color="text.secondary">
               <b>{post.subreddit_name_prefixed}</b> {bull} Posted by {post.author}{" "}
-              {bull} {diff} days ago
+              {bull} {getAge(post)}
             </Typography>
             <Typography
               sx={{ fontSize: 16, mt: 1 }}
