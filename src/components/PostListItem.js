e38a3e8f2b7eb29f66dacec32f40800b22e48d7c
lwieckowski <lwieckowski@outlook.com>
2023-10-15 12:23:08 +0200

    Add thumbnail to post search results

--- src/components/PostListItem.js
+++ src/components/PostListItem.js
@@ -1,4 +1,4 @@
-import { CardActionArea, Card, CardContent, Typography } from "@mui/material";
+import { Box, CardActionArea, Card, CardContent, Typography, CardMedia } from "@mui/material";
 
 const bull = "\u2022";
 
@@ -8,25 +8,33 @@ export function PostListItem({ post }) {
 
   return (
     <Card sx={{ minWidth: 275, mt: 1 }}>
-      <CardActionArea>
-        <CardContent>
-          <Typography sx={{ fontSize: 11 }} color="text.secondary">
-            <b>{post.subreddit_name_prefixed}</b> {bull} Posted by {post.author}{" "}
-            {bull} {diff} days ago
-          </Typography>
-          <Typography
-            sx={{ fontSize: 14, mt: 1 }}
-            color="text.secondary"
-            variant="body2"
-            fontWeight="fontWeightBold"
-          >
-            {post.title}
-          </Typography>
-          <Typography sx={{ fontSize: 11, mt: 1 }} color="text.secondary">
-            {post.ups} upvotes {bull} {post.num_comments} comments {bull}{" "}
-            {post.total_awards_received} awards
-          </Typography>
-        </CardContent>
+      <CardActionArea sx={{display: 'flex', justifyContent: 'space-between'}}>
+        <Box>
+          <CardContent>
+            <Typography sx={{ fontSize: 13 }} color="text.secondary">
+              <b>{post.subreddit_name_prefixed}</b> {bull} Posted by {post.author}{" "}
+              {bull} {diff} days ago
+            </Typography>
+            <Typography
+              sx={{ fontSize: 16, mt: 1 }}
+              color="text.secondary"
+              variant="body2"
+              fontWeight="fontWeightBold"
+            >
+              {post.title}
+            </Typography>
+            <Typography sx={{ fontSize: 13, mt: 1 }} color="text.secondary">
+              {post.ups} upvotes {bull} {post.num_comments} comments {bull}{" "}
+              {post.total_awards_received} awards
+            </Typography>
+          </CardContent>
+        </Box>
+        <Box>
+        <CardMedia
+            sx={{ width: 120, height: 120, m: 1, objectFit: "contain" }}
+            image={post.thumbnail}
+        />
+        </Box>
       </CardActionArea>
     </Card>
   );
